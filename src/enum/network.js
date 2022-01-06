a3c2f8e53a33beecc50504540b60b4d5ad38cc4f
ipyramiddev <goldendriver0731@gmail.com>
2022-01-06 14:01:04 +0700

    add network list #45

---
+++ src/enum/network.js
@@ -0,0 +1,38 @@
+export const networkList = {
+  1: {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    icon: 'h-eth.svg',
+    type: 2
+  },
+  3: {
+    name: 'Ropsten',
+    symbol: 'ETH',
+    icon: 'h-eth.svg',
+    type: null
+  },
+  56: {
+    name: 'BSC',
+    symbol: 'BNB',
+    icon: 'h-bsc.svg',
+    type: 1
+  },
+  97: {
+    name: 'BSC Test Net',
+    symbol: 'BNB',
+    icon: 'h-bsc.svg',
+    type: null
+  },
+  137: {
+    name: 'Matic Network (Polygon)',
+    symbol: 'MATIC',
+    icon: 'h-matic.svg',
+    type: null
+  },
+  43114: {
+    name: 'Avalanche Mainnet C-Chain',
+    symbol: 'AVAX',
+    icon: 'h-avalanche.svg',
+    type: null
+  }
+}
