8ea3d0e3c5a366f8a134aa95e157b4908808c34a
ipyramiddev <goldendriver0731@gmail.com>
2022-02-14 11:33:52 +0700

    add vuex persistence mechanism #89

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistedState from 'vuex-persistedstate'
+import SecureLS from 'secure-ls'
 
 import web3 from './modules/web3'
 import account from './modules/account'
@@ -7,7 +9,29 @@ import payment from './modules/payment'
 
 Vue.use(Vuex);
 
+var ls = new SecureLS({ isCompression: false })
+
 const store = new Vuex.Store({
+  plugins: [
+    VuexPersistedState({
+      key: 'state',
+      paths: [
+        'web3.chainId',
+        'web3.provider',
+        'account',
+        'payment',
+        'humberger',
+        'modal',
+        'theme',
+        'invoicePage'
+      ],
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key)
+      }
+    })
+  ],
   modules: {
     web3,
     account,
@@ -65,7 +89,7 @@ const store = new Vuex.Store({
     },
     currentPath(state, { invoicePage }) {
       state.invoicePage = invoicePage;
-    },
+    }
   }
 })
 
