57e45d3e35deffe4c86c5625d2d3cd733fdbaaec
ipyramiddev <goldendriver0731@gmail.com>
2022-03-03 09:08:39 +0700

    update avalanche and matic

--- src/contracts/merchant_factory/networks/matic/develoment.js
+++ src/contracts/merchant_factory/networks/matic/develoment.js
@@ -2,5 +2,5 @@ import { NETWORKS } from '@/constants'
 
 export default {
   chainId: NETWORKS[80001].chainId,
-  address: '0x8a3dd4008703d036583b79c0f33ae7c637933bda'
+  address: '0xc690fC764113E8c8D2d77f6BD4f89eCfa89d86FE'
 }
