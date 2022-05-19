3be6b8f7e80657bcc3a87f05bf1aee23d68df127
ipyramiddev <goldendriver0731@gmail.com>
2022-05-19 17:03:38 +0900

    add: bisinessPage

---
+++ src/components/organisms/lp/lpFooter.vue
@@ -0,0 +1,253 @@
+<template>
+  <footer>
+    <div class="footer-wrap">
+      <div class="lp-footer_left">
+        <div class="lp-first_icon">
+          <img src="@/assets/images/lp/logo.png" alt="" />
+          <h4>Slash Payment</h4>
+        </div>
+        <div class="lp-footer_link"><p><a href="">Terms of Service</a> | <a href="">Privacy Terms</a></p></div>
+        <div class="lp-footer_info">
+          <p>
+            <span>SLASH FINTECH LIMITED（BVI）</span>
+            <br>
+            3rd Floor, J & C Building, Road
+            <br>
+            Town, Tortola, British Virgin Islands, VG1110
+          </p>
+        </div>
+        <div class="lp-footer_info">
+          <p>
+            <span>SLASH FINTECH LIMITED（CANADA）</span>
+            <br>
+            1700-777 DUNSMUIR ST. 
+            <br>
+            VANCOUVER, BRITISH COLUMBIA V7Y1K4
+            <br>
+            Money Services Business (MSB) Registration number is M22785834
+          </p>
+        </div>
+      </div>
+      <div class="lp-footer_right">
+        <div class="lp-footer_rightbox">
+          <div class="box start">
+            <h4>Developers</h4>
+            <p><a href="" target="_blank">Integrations</a></p>
+            <p><a href="" target="_blank">Plugins</a></p>
+            <p><a href="" target="_blank">Docs</a></p>
+            <p><a href="" target="_blank">Slash Developers</a></p>
+          </div>
+          <div class="box">
+            <h4>Fee & Ecosystem</h4>
+            <p><a href="#fee-ecosystem">Fee & Ecosystem</a></p>
+            <p><a href="#donation">Donation Program</a></p>
+            <p><a href="#token">Slash Token</a></p>
+          </div>
+          <div class="box end">
+            <h4>Other</h4>
+            <p><a href="#roadmap">Road Map</a></p>
+            <p><a href="#developers">Referral</a></p>
+            <p><a href="#contact">Contact</a></p>
+          </div>
+        </div>
+        <div class="footer_icon-wrap">
+          <div class="footer_icon">
+            <figure>
+              <a href="">
+                <img v-if="$store.state.theme == 'dark'" src="@/assets/images/lp/twitter.svg" alt="" />
+                <img v-if="$store.state.theme == 'light'" src="@/assets/images/lp/twitter-l.svg" alt="" />
+              </a>
+            </figure>
+            <figure>
+              <a class="disable" href=""><img src="@/assets/images/lp/telegram.svg" alt="" /></a>
+            </figure>
+            <figure>
+              <a class="disable" href=""><img src="@/assets/images/lp/discord.svg" alt="" /></a>
+            </figure>
+            <figure>
+              <a href="">
+                <img v-if="$store.state.theme == 'dark'" src="@/assets/images/lp/arroba.svg" alt="" />
+                <img v-if="$store.state.theme == 'light'" src="@/assets/images/lp/arroba-l.svg" alt="" />
+              </a>
+            </figure>
+            <figure>
+              <a href="">
+                <img v-if="$store.state.theme == 'dark'" src="@/assets/images/lp/gitbook-icon.svg" alt="" />
+                <img v-if="$store.state.theme == 'light'" src="@/assets/images/lp/gitbook-icon-l.svg" alt="" />
+              </a>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p class="copy">
+      @ 2022 Copyrights by SLASH FINTECH LIMITED . All Rights Reserved.
+    </p>
+  </footer>
+</template>
+
+<script>
+
+  export default {
+    name: 'lpFooter',
+    methods: {
+
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/style.scss';
+
+footer{
+  .footer-wrap{
+    padding:90px 0 62px;
+    display:flex;
+    justify-content:space-between;
+    text-align:left;
+    max-width: 1030px;
+    width:90%;
+    margin:0 auto;
+    font-weight: 400;
+    @include media(sp) {
+      display:block;
+      padding:35px 0;
+    }
+    .lp-first_icon{
+      display:flex;
+      align-items:center;
+      margin-bottom:15px;
+      width: 100%;
+      @include media(sp) {
+        margin-bottom: 40px;
+      }
+      img{
+        width:40px;
+        margin-right:15px;
+      }
+      h4{
+        font-size:30px;
+        background: $gradation-light;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    }
+    .lp-footer_left{
+      @include media(sp) {
+        width:100%;
+        margin-bottom: 40px;
+      }
+      p{
+        font-size:15px;
+      }
+      .lp-footer_link{
+        margin-top:10px;
+        @include media(sp) {
+          margin-bottom:35px;
+        }
+      }
+      .lp-footer_info{
+        margin-top:60px;
+        @include media(sp) {
+          margin-top:24px;
+        }
+        p{
+          font-size: 12px;
+          font-weight: 200;
+          @include media(sp) {
+            font-size: 10px;
+          }
+        }
+        span{
+          font-size: 16px;
+          font-weight: 400;
+          margin-bottom: 8px;
+          display: inline-block;
+          @include media(sp) {
+            font-size: 14px;
+          }
+        }
+      }
+    }
+    @include media(sp) {
+      .lp-footer_info{
+        font-size:15px;
+        margin-top:25px;
+      }
+    }
+
+    .lp-footer_right{
+      width: 60%;
+      @include media(sp) {
+        width:100%;
+      }
+      .lp-footer_rightbox{
+        display:flex;
+        justify-content: space-between;
+        @include media(sp) {
+          display:block;
+        }
+        .box{
+          @include media(sp) {
+            margin-left:0;
+            border-bottom:1px solid #554B6A;
+            padding: 24px 0;
+            &.start{
+              border-top:1px solid #554B6A;
+            }
+            &.end{
+              margin-bottom: 62px;
+            }
+          }
+          h4{
+            font-weight:600;
+            margin-bottom:15px;
+          }
+          p{
+            font-weight:300;
+            margin-bottom:11px;
+            font-size: 19px;
+          }
+        }
+      }
+      .footer_icon-wrap{
+        width: 50%;
+        margin-top: 120px;
+        margin-right: 0;
+        margin-left: auto;
+        @include media(sp) {
+          width: 80%;
+          margin: 20px auto;
+        }
+      }
+      .footer_icon{
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        .disable{
+          opacity: .5;
+        }
+        figure{
+          width: 20%;
+          text-align: right;
+          @include media(sp) {
+            text-align: center;
+          }
+          img{
+            width: 30px;
+          }
+        }
+      }
+    }
+  }
+  .copy{
+    font-weight: 200;
+    font-size: 16px;
+    padding-bottom: 32px;
+    @include media(sp) {
+      font-size: 10px;
+      padding-bottom: 48px;
+    }
+  }
+}
+</style>
---
+++ src/components/templates/BrandingIndex.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+    <lp-header />
+    <article class="lp display">
+      <section>
+        <div class="title">
+          <h1>
+            Branding
+          </h1>
+          <p>
+            Latest update: March, 2022
+          </p>
+        </div>
+        <div class="sub-title">
+          <h2>
+            Slash Payment Brand
+          </h2>
+          <p class="dsc-title">
+            What is Slash Payment?
+          </p>
+          <p class="dsc">
+            Slash Payment is Merchants can automatically accept online TOKEN payments
+            <br>
+            from customers using the Web3 wallet.
+          </p>
+        </div>
+        <div class="logo">
+          <div class="contents-title">
+            <h3>
+              Logomark
+            </h3>
+            <div class="add-flex">
+              <figure>
+                <img src="@/assets/images/lp/logo-l.png" alt="">
+              </figure>
+              <figure>
+                <img src="@/assets/images/lp/logo-m.png" alt="">
+              </figure>
+            </div>
+          </div>
+          <div class="logo-clearspace">
+            <h4>
+              Clearspace
+            </h4>
+            <div class="add-flex">
+              <figure>
+                <img src="@/assets/images/lp/clearspace-l.png" alt="">
+              </figure>
+              <figure>
+                <img src="@/assets/images/lp/clearspace-m.png" alt="">
+              </figure>
+            </div>
+          </div>
+        </div>
+        <div class="usecase">
+          <div class="contents-title">
+            <h3>
+              Use case
+            </h3>
+            <div class="usecase-block">
+              <h4>
+                STANDARD BUTTON
+              </h4>
+              <figure>
+                <img src="@/assets/images/lp/standard-button.png" alt="">
+              </figure>
+            </div>
+            <div class="usecase-block">
+              <h4>
+                Settlement indication
+              </h4>
+              <figure>
+                <img src="@/assets/images/lp/indication.png" alt="">
+              </figure>
+            </div>
+          </div>
+        </div>
+        <div class="misuse">
+          <div class="contents-title">
+            <h3>
+              Misuse
+              <span>
+                <img src="@/assets/images/lp/close.png" alt="">
+              </span>
+            </h3>
+          </div>
+          <div class="add-flex">
+            <div class="mususe-item">
+              <figure>
+                <img src="@/assets/images/lp/misuse-01.png" alt="">
+              </figure>
+              <p>
+                Don’t adjust the width
+              </p>
+            </div>
+            <div class="mususe-item">
+              <figure>
+                <img src="@/assets/images/lp/misuse-02.png" alt="">
+              </figure>
+              <p>
+                Don’t add other visual effects
+              </p>
+            </div>
+          </div>
+          <div class="add-flex">
+            <div class="mususe-item">
+              <figure>
+                <img src="@/assets/images/lp/misuse-03.png" alt="">
+              </figure>
+              <p>
+                Don’t change color
+              </p>
+            </div>
+            <div class="mususe-item">
+              <figure>
+                <img src="@/assets/images/lp/misuse-04.png" alt="">
+              </figure>
+              <p>
+                Don't change the ratio
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="another">
+          <div class="contents-title">
+            <h3>
+              Another Logos
+            </h3>
+          </div>
+          <p>
+            Please use this one depending on your application.
+          </p>
+          <div class="another-logo">
+            <figure>
+              <img src="@/assets/images/lp/another-logo.png" alt="">
+            </figure>
+            <figure>
+              <img src="@/assets/images/lp/another-full-logo.png" alt="">
+            </figure>
+          </div>
+        </div>
+        <div class="download">
+          <div class="contents-title">
+            <h3>
+              Download Assets
+            </h3>
+          </div>
+        </div>
+      </section>
+      <lp-footer />
+    </article>
+  </div>
+</template>
+
+
+<script>
+import LpHeader from "@/components/organisms/lp/lpHeader";
+import LpFooter from "@/components/organisms/lp/lpFooter";
+
+export default {
+  name: "payment",
+  data() {
+    return {
+    };
+  },
+  components: {
+    LpHeader,
+    LpFooter,
+  },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/assets/scss/style.scss";
+</style>
+
