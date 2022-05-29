87d3e40a899a0e84bff872af6388816506d85524
ipyramiddev <goldendriver0731@gmail.com>
2022-05-29 18:49:06 +0700

    Add ETH Ropsten NetWork factory contract #245

--- src/contracts/merchant_factory/networks/ethereum/develoment.js
+++ src/contracts/merchant_factory/networks/ethereum/develoment.js
@@ -2,5 +2,5 @@ import { NETWORKS } from '@/constants'
 
 export default {
   chainId: NETWORKS[3].chainId,
-  address: '0xbD1C4729f0abCF82F1212DcBa0FF38e501E09a7D'
+  address: '0x6E9F57B5Cd0564e5DF2f66c2D9E03cc6e1A424A8'
 }
