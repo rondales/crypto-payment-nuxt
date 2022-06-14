85a91ece3b3e987e3fe6a2e49ec2473e5fbcf146
ipyramiddev <goldendriver0731@gmail.com>
2022-06-14 17:23:30 +0700

    add result page components #369

---
+++ src/components/organisms/PaymentResultFailure.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="payment-status mt-3 mb-3">
+      <div>
+        <img class="mb-2" src="@/assets/images/multiply.svg" alt="failure">
+        <p class="payment-status_desc mb-2">
+          The transaction cannot succeed due to error: execution
+          <br>
+          Check the reason for the reverted from Explorer.
+        </p>
+      </div>
+      <a v-if="hasReturnUrl" class="payment-status_btn" target="_blank" :href="urls.explorer">
+        View on explorer
+        <img src="@/assets/images/link-icon.svg" alt="another">
+      </a>
+    </div>
+    <a v-if="hasReturnUrl" :href="urls.failure">
+      <button class="btn __g __l mb-2">
+        Back to Payee’s Services
+      </button>
+    </a>
+    <a v-else target="_blank" :href="urls.explorer">
+      <button class="btn __g __l mb-2">
+        View on explorer
+        <img class="new-tab-icon" src="@/assets/images/link-icon.svg" alt="another">
+      </button>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentResultFailure',
+  props: { urls: Object },
+  computed: {
+    hasReturnUrl() {
+      return (urls.failure)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/style.scss';
+
+.payment-status{
+  text-align: center;
+  margin: auto;
+  &_title{
+    font-size: 18px;
+    font-weight: 100;
+  }
+  &_desc{
+    font-size: 10px;
+    font-weight: 100;
+  }
+  &_btn{
+    font-size: 12px;
+    font-weight: 100;
+    cursor: pointer;
+    background: $gradation-pale;
+    padding: 4px 16px;
+    border-radius: 10px;
+    color: #fff;
+    img{
+      margin-left: 4px;
+      vertical-align: middle;
+    }
+  }
+}
+.new-tab-icon {
+  padding: 0!important;
+}
+</style>
