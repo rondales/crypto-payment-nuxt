dea75f4ee13671461ff6687e7b63397922796b62
ipyramiddev <goldendriver0731@gmail.com>
2022-06-29 16:31:44 +0700

    Seprate contract in production testnet and staging #401

--- src/contracts/merchant_factory/networks/ethereum/production.js
+++ src/contracts/merchant_factory/networks/ethereum/production.js
@@ -1,6 +1,12 @@
 import { NETWORKS } from '@/constants'
 
 export default {
-  chainId: NETWORKS[1].chainId,
-  address: null
+  mainnet: {
+    chainId: NETWORKS[1].chainId,
+    address: null
+  },
+  testnet: {
+    chainId: NETWORKS[5].chainId,
+    address: '0x7b0afD36918c080AdF13B5f33c0F1Cecbc77dDA0'
+  }
 }
