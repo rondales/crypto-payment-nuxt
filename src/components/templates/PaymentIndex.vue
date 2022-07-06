c935b654628c6a60ac5262916bb4ae1c17c73470
ipyramiddev <goldendriver0731@gmail.com>
2022-07-06 14:32:41 +0900

    modify payment wrapper box on sp screen

--- src/components/templates/PaymentIndex.vue
+++ src/components/templates/PaymentIndex.vue
@@ -1,39 +1,53 @@
 <template>
   <div class="slash-bg">
-    <Header
-      :width="windowWidth"
-      @switchColorTheme="switchColorTheme"
-    />
+    <Header :width="windowWidth" @switchColorTheme="switchColorTheme" />
     <div v-if="initializing" class="payment">
       <div class="payment_initializing">
-        <img class="mb-2 spin" src="@/assets/images/loading.svg" alt="processing">
-        <p class="title">
-          Loading...
-        </p>
+        <img
+          class="mb-2 spin"
+          src="@/assets/images/loading.svg"
+          alt="processing"
+        />
+        <p class="title">Loading...</p>
       </div>
     </div>
     <div v-else class="payment">
       <div class="menu-nav" v-if="showMenu">
         <div class="menu-nav_top">
-          <img src="@/assets/images/menu.svg">
+          <img src="@/assets/images/menu.svg" />
         </div>
         <div class="menu-nav_body">
           <!-- <a href="/">
             Cookie settings
           </a> -->
-          <a href="https://slash-fi.gitbook.io/docs/whitepaper/slash-project-white-paper" target="_blank">
+          <a
+            href="https://slash-fi.gitbook.io/docs/whitepaper/slash-project-white-paper"
+            target="_blank"
+          >
             About Slash.fi
           </a>
-          <a href="https://slash-fi.gitbook.io/docs/support/help-center" target="_blank">
+          <a
+            href="https://slash-fi.gitbook.io/docs/support/help-center"
+            target="_blank"
+          >
             FAQ
           </a>
-          <a href="https://slash-fi.gitbook.io/docs/support/terms-of-use" target="_blank">
+          <a
+            href="https://slash-fi.gitbook.io/docs/support/terms-of-use"
+            target="_blank"
+          >
             Terms of Service
           </a>
-          <a href="https://slash-fi.gitbook.io/docs/support/data-protection-and-privacy-policy" target="_blank">
+          <a
+            href="https://slash-fi.gitbook.io/docs/support/data-protection-and-privacy-policy"
+            target="_blank"
+          >
             Privacy Policy
           </a>
-          <a href="https://slash-fi.gitbook.io/docs/support/anti-money-laundering-policy" target="_blank">
+          <a
+            href="https://slash-fi.gitbook.io/docs/support/anti-money-laundering-policy"
+            target="_blank"
+          >
             AML Policy
           </a>
         </div>
@@ -47,54 +61,47 @@
         <div>
           <p class="payment_Receiver mb-1">
             Payee：{{ receiver }}
-            <img v-if="isVerifiedDomain" :src="domainVerifiedIcon">
+            <img v-if="isVerifiedDomain" :src="domainVerifiedIcon" />
           </p>
-          <p class="payment_invoice-id">
-            Invoice ID：{{ invoiceId }}
-          </p>
+          <p class="payment_invoice-id">Invoice ID：{{ invoiceId }}</p>
         </div>
-          <router-view />
+        <router-view />
       </div>
       <div v-if="loading" class="loading add-flex j-center a-center">
-        <img class="spin" src="@/assets/images/loading.svg">
+        <img class="spin" src="@/assets/images/loading.svg" />
       </div>
     </div>
     <div v-if="showFooterMenu" class="sp">
       <div class="fixed add-flex j-between a-center">
-        <button class="btn __pg __s sp-fixed"  @click="openModal('wallet-modal', 'small')">
+        <button
+          class="btn __pg __s sp-fixed"
+          @click="openModal('wallet-modal', 'small')"
+        >
           <span class="icon-wrap">
-            <img src="@/assets/images/wallet-connect_w.svg">
+            <img src="@/assets/images/wallet-connect_w.svg" />
           </span>
           Connect to a wallet
         </button>
         <button class="btn __pg __s sp-fixed" @click="copyLink">
           <span class="icon-wrap">
-            <img src="@/assets/images/link.svg">
+            <img src="@/assets/images/link.svg" />
           </span>
           Copy URL
         </button>
         <span class="toggle-theme">
           <button
-            :class="[
-              'theme-button',
-              '--light',
-              { 'is-active': isLightTheme },
-            ]"
+            :class="['theme-button', '--light', { 'is-active': isLightTheme }]"
             @click="switchColorTheme(lightTheme)"
             v-if="isDarkTheme"
           >
-            <img src="@/assets/images/light.svg" alt="">
+            <img src="@/assets/images/light.svg" alt="" />
           </button>
           <button
-            :class="[
-              'theme-button',
-              '--dark',
-              { 'is-active': isDarkTheme },
-            ]"
+            :class="['theme-button', '--dark', { 'is-active': isDarkTheme }]"
             @click="switchColorTheme(darkTheme)"
             v-if="isLightTheme"
           >
-            <img src="@/assets/images/dark.svg" alt="">
+            <img src="@/assets/images/dark.svg" alt="" />
           </button>
         </span>
       </div>
@@ -104,121 +111,124 @@
 
 
 <script>
-import Header from '@/components/organisms/header'
-import PaymentTop from '@/components/organisms/PaymentTop'
-import { DARK_THEME, LIGHT_THEME } from '@/constants'
+import Header from "@/components/organisms/header";
+import PaymentTop from "@/components/organisms/PaymentTop";
+import { DARK_THEME, LIGHT_THEME } from "@/constants";
 
 export default {
-  name: 'PaymentIndex',
+  name: "PaymentIndex",
   components: {
     Header,
     PaymentTop,
   },
   props: [
-    'colorTheme',
-    'showFooterMenu',
-    'receiver',
-    'isVerifiedDomain',
-    'invoiceId',
-    'base64VuexData',
-    'initializing'
+    "colorTheme",
+    "showFooterMenu",
+    "receiver",
+    "isVerifiedDomain",
+    "invoiceId",
+    "base64VuexData",
+    "initializing",
   ],
   data() {
     return {
       windowWidth: window.innerWidth,
       showMenu: false,
-      loading: false
-    }
+      loading: false,
+    };
   },
   computed: {
     darkTheme() {
-      return DARK_THEME
+      return DARK_THEME;
     },
     lightTheme() {
-      return LIGHT_THEME
+      return LIGHT_THEME;
     },
     domainVerifiedIcon() {
       return this.isDarkTheme
-        ? require('@/assets/images/domain-verified.svg')
-        : require('@/assets/images/domain-verified-l.svg')
+        ? require("@/assets/images/domain-verified.svg")
+        : require("@/assets/images/domain-verified-l.svg");
     },
     isDarkTheme() {
-      return this.colorTheme === this.darkTheme
+      return this.colorTheme === this.darkTheme;
     },
     isLightTheme() {
-      return this.colorTheme === this.lightTheme
+      return this.colorTheme === this.lightTheme;
     },
     isShowMenu() {
-      return this.showMenu
-    }
+      return this.showMenu;
+    },
   },
   methods: {
     openModal(target, size) {
-      this.$emit('openModal', target, size)
+      this.$emit("openModal", target, size);
     },
     switchColorTheme(color) {
-      this.$emit('switchColorTheme', color)
+      this.$emit("switchColorTheme", color);
     },
     handleWindowResize() {
-      this.windowWidth = window.innerWidth
+      this.windowWidth = window.innerWidth;
     },
     copyLink() {
-      this.$store.dispatch('account/copied')
-      const uri = new URL(window.location.href)
-      const token = this.$route.params.token
-      this.$clipboard(`${uri.protocol}//${uri.host}/payment/result/${token}?rcpt=1`)
+      this.$store.dispatch("account/copied");
+      const uri = new URL(window.location.href);
+      const token = this.$route.params.token;
+      this.$clipboard(
+        `${uri.protocol}//${uri.host}/payment/result/${token}?rcpt=1`
+      );
     },
     toggleMenu(state) {
-      this.showMenu = state
-    }
+      this.showMenu = state;
+    },
   },
   mounted() {
-    window.addEventListener('resize', this.handleWindowResize)
+    window.addEventListener("resize", this.handleWindowResize);
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.handleResize)
-  }
-}
+    window.removeEventListener("resize", this.handleResize);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/style.scss';
-  .slash-bg{
+@import "@/assets/scss/style.scss";
+.slash-bg {
+  width: 100%;
+  min-height: 120vh;
+  @include media(sp) {
+    padding-top: 55px;
+    min-height: 100vh;
+  }
+  position: relative;
+  &::before {
+    content: "";
+    background: url(/assets/images/slash-bg.png) no-repeat center center;
     width: 100%;
-    min-height: 120vh;
+    height: 100vh;
+    position: fixed;
+    top: 100px;
+    z-index: 0;
     @include media(sp) {
-      min-height: 140vh;
-    }
-    position: relative;
-    &::before{
-      content: "";
-      background: url(/assets/images/slash-bg.png) no-repeat center center;
-      width: 100%;
-      height: 100vh;
-      position: fixed;
-      top: 100px;
-      z-index: 0;
-      @include media(sp) {
-        top: 70px;
-      }
+      top: 70px;
     }
   }
+}
 .theme--light {
-  .slash-bg{
-    &::before{
+  .slash-bg {
+    &::before {
       content: "";
       background: url(/assets/images/slash-bg-l.png) no-repeat center center;
     }
   }
 }
-.home{
+.home {
   text-align: center;
 }
-.payment{
+.payment {
   position: absolute;
   top: 60vh;
   left: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
   box-shadow: var(--color_shadow);
   width: 100%;
   max-width: 36.1rem;
@@ -226,9 +236,14 @@ export default {
   border-radius: 8px;
   background: var(--color_bg);
   @include media(sp) {
-    top: calc(50% + 12rem);
+    // top: calc(50% + 12rem);
+    position: relative;
+    top: 0;
+    left: 0;
+    transform: translate(0%, 0%);
+    margin: 3rem auto;
   }
-  &::before{
+  &::before {
     content: "Slash.fi Web3 Payment ®︎";
     font-size: 11px;
     color: var(--color_font);
@@ -237,9 +252,9 @@ export default {
     bottom: -60px;
     left: 50%;
     transform: translate(-50%, 0);
-    opacity: .7;
+    opacity: 0.7;
   }
-  .menu-nav{
+  .menu-nav {
     position: absolute;
     top: 0;
     left: 0;
@@ -250,11 +265,11 @@ export default {
     z-index: 1;
     border-radius: 10px;
   }
-  .menu-nav_top{
+  .menu-nav_top {
     margin-bottom: 32px;
   }
-  .menu-nav_body{
-    a{
+  .menu-nav_body {
+    a {
       display: block;
       font-size: 18px;
       font-weight: 100;
@@ -272,7 +287,7 @@ export default {
     }
   }
   .payment_Receiver,
-  .payment_invoice-id{
+  .payment_invoice-id {
     font-weight: 400;
     font-size: 15px;
   }
@@ -280,14 +295,14 @@ export default {
     position: absolute;
     top: 0;
     left: 0;
-    width:100%;
+    width: 100%;
     height: 100%;
-    background: rgba(0,0,0,.6);
+    background: rgba(0, 0, 0, 0.6);
     border-radius: 8px;
     z-index: 9999;
   }
 }
-.fixed{
+.fixed {
   position: fixed;
   bottom: 0;
   left: 50%;
@@ -295,15 +310,15 @@ export default {
   width: 100%;
   padding: 16px;
   background: var(--color_darken);
-  .btn{
+  .btn {
     font-size: 12px;
     height: 4.2rem;
     line-height: 4.2rem;
     padding: 0 1.4rem;
-    .icon-wrap{
+    .icon-wrap {
       margin-right: 4px;
     }
-    img{
+    img {
       vertical-align: middle;
       width: 18px;
       height: 18px;
