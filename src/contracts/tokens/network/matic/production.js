b2c8740d5633d3366dfb263b1f2ef64cd616688d
ipyramiddev <goldendriver0731@gmail.com>
2022-07-27 15:12:49 +0700

    Add DAI,JPYC to contracts/tokens folder

--- src/contracts/tokens/network/matic/production.js
+++ src/contracts/tokens/network/matic/production.js
@@ -2,6 +2,8 @@ import { NETWORKS } from '@/constants'
 import Usdt from '../../abis/matic/usdt'
 import Usdc from '../../abis/matic/usdc'
 import Wmatic from '../../abis/matic/wmatic'
+import Dai from '../../abis/matic/dai'
+import Jpyc from '../../abis/matic/jpyc'
 
 const url = NETWORKS[137].scanUrl + '/address'
 
@@ -37,5 +39,21 @@ export default {
     abi: Wmatic.abi,
     url: `${url}/0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270`,
     icon: require('@/assets/images/symbol/wmatic.svg')
+  },
+  DAI: {
+    name: 'Maker DAO',
+    symbol: 'DAI',
+    address: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+    abi: Dai.abi,
+    url: `${url}/0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063`,
+    icon: require('@/assets/images/symbol/dai.svg')
+  },
+  JPYC: {
+    name: 'JPY Coin',
+    symbol: 'JPYC',
+    address: '0x6AE7Dfc73E0dDE2aa99ac063DcF7e8A63265108c',
+    abi: Jpyc.abi,
+    url: `${url}/0x6AE7Dfc73E0dDE2aa99ac063DcF7e8A63265108c`,
+    icon: require('@/assets/images/symbol/jpyc.svg')
   }
 }
