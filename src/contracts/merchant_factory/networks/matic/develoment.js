764d22345ff177a0314664bf6d9781d2ce475dcf
ipyramiddev <goldendriver0731@gmail.com>
2022-08-08 18:20:19 +0700

    Integrate 2022/08/08 contract #462

--- src/contracts/merchant_factory/networks/matic/develoment.js
+++ src/contracts/merchant_factory/networks/matic/develoment.js
@@ -2,5 +2,5 @@ import { NETWORKS } from '@/constants'
 
 export default {
   chainId: NETWORKS[80001].chainId,
-  address: '0xBEB54A97845aC169836364688536562bffAe9276'
+  address: '0xA97357A1718017765b1A8627d8e589167F2d7e10'
 }
