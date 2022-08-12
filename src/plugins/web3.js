60fe2f2568aa531997ab394d88daac542c4cef25
ipyramiddev <goldendriver0731@gmail.com>
2022-08-12 21:03:02 +0700

    Fix path logic

--- src/plugins/web3.js
+++ src/plugins/web3.js
@@ -488,7 +488,7 @@ const sendPaymentTransaction = async function(
   const reservedParam = '0x'
   let path
   if ((chainId == '5' || chainId == '1') && paymentRequestSymbol == 'WETH') {
-    if(token.address != null || token.address != wrappedToken.address) {
+    if(token.address === null || token.address === wrappedToken.address) {
       path = [wrappedToken.address, requestToken.address]
     } else {
       path = [token.address, requestToken.address]
