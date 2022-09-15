78f28a6a070d6bd908fa9c8bd7d545b51d0fd2b2
ipyramiddev <goldendriver0731@gmail.com>
2022-09-15 12:33:06 +0700

    fix bug in connect wallet for payment modal #441

--- src/components/molecules/walletModal.vue
+++ src/components/molecules/walletModal.vue
@@ -7,14 +7,12 @@
           size="l"
           text="MetaMask"
           icon="wallet-metamask"
-          :loading="loadingMeta"
         />
         <PaymentButton
           @click.native="showWalletConnectCautionModal()"
           size="l"
           text="WalletConnect"
           icon="wallet-walletconnect"
-          :loading="loadingWallet"
         />
       </div>
       <div class="d-btnwrap bottomCloseBtn">
@@ -27,35 +25,6 @@
         />
       </div>
     </PaymentModal>
-    <!-- <div :class="classes">
-      <div class="header">
-        <h3 class="header__title">Select a Wallet</h3>
-      </div>
-      <div class="body">
-        <button
-          class="btn __m __pg icon-right full"
-          @click="connector(METAMASK)"
-        >
-          <span class="btn-icon">
-            <img src="@/assets/images/metamask-fox.svg" />
-          </span>
-          MetaMask
-        </button>
-        <button
-          class="btn __m __pg icon-right full"
-          @click="showWalletConnectCautionModal()"
-        >
-          <span class="btn-icon">
-            <img src="@/assets/images/wallet-connect_w.svg" />
-          </span>
-          WalletConnect
-        </button>
-      </div>
-      <button class="close" @click="hideModal()">
-        <img src="@/assets/images/cross.svg" />
-        閉じる
-      </button>
-    </div> -->
   </div>
 </template>
 
@@ -131,85 +100,4 @@ export default {
     }
   }
 }
-// .modal-box {
-//   border-radius: 10px;
-//   position: fixed;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
-//   background: var(--color_bg);
-//   @include media(pc) {
-//     &.--small {
-//       width: 470px;
-//     }
-//     &.--medium {
-//       width: 760px;
-//     }
-//   }
-//   @include media(sp) {
-//     width: calc(100vw - 32px);
-//   }
-// }
-// .header {
-//   @include media(pc) {
-//     padding: 24px;
-//     &__title {
-//       font-size: 2.5rem;
-//       margin-bottom: 2rem;
-//     }
-//     &__desc {
-//       font-size: 2rem;
-//     }
-//   }
-//   @include media(sp) {
-//     padding: 18px;
-//     &__title {
-//       font-size: 1.7rem;
-//     }
-//   }
-//   &__title {
-//     font-weight: 500;
-//   }
-//   &__desc {
-//     font-weight: 100;
-//   }
-// }
-// .close {
-//   position: absolute;
-//   width: 16px;
-//   height: 16px;
-//   font-size: 0;
-
-//   @include media(pc) {
-//     top: 30px;
-//     right: 24px;
-//   }
-//   @include media(sp) {
-//     top: 24px;
-//     right: 24px;
-//   }
-// }
-// .body {
-//   @include media(pc) {
-//     padding: 24px 24px 40px;
-//   }
-//   @include media(sp) {
-//     padding: 16px 12px 48px;
-//   }
-//   // .btn {
-//   //   &.__m {
-//   //     background: $gradation-pale;
-//   //   }
-//   // }
-// }
-// .footer {
-//   text-align: center;
-
-//   @include media(pc) {
-//     padding: 0 40px 40px;
-//   }
-//   @include media(sp) {
-//     padding: 0 32px 32px;
-//   }
-// }
 </style>
