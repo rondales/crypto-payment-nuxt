3f65be9a68ca9c0978a09c64ffdeebf60eb21ea8
ipyramiddev <goldendriver0731@gmail.com>
2022-10-20 08:58:02 +0700

    Fix minior bug #721

--- src/components/organisms/PaymentResult.vue
+++ src/components/organisms/PaymentResult.vue
@@ -322,7 +322,7 @@ export default {
     },
     handleAddMerchantSiteRedirectParam() {
       if (this.status == STATUS_RESULT_FAILURE || this.status == STATUS_RESULT_SUCCESS) {
-        if (this.isMetamaskBrowser) {
+        if (this.isMetamaskBrowser && (this.successReturnUrl || this.failureReturnUrl)) {
           history.pushState(
             {},
             null,
