13a7ec6a4dbf54e0c6607497583d33b33c98274c
ipyramiddev <goldendriver0731@gmail.com>
2023-01-20 14:12:57 +0700

    Revert "fix copylight year"

--- src/components/organisms/lp/lpFooter.vue
+++ src/components/organisms/lp/lpFooter.vue
@@ -68,7 +68,7 @@
       </ul>
       <p class="copy">
         <span
-          >@ {{ copyrightYear }} Copyrights by <a href="https://slash.vision/">SLASH VISION.</a> All Rights
+          >@ 2022 Copyrights by <a href="https://slash.vision/">SLASH VISION.</a> All Rights
           Reserved.</span
         >
       </p>
@@ -77,7 +77,6 @@
 </template>
 
 <script>
-import moment from 'moment'
 import LpIcon from "@/components/templates/LpParts/Icon";
 export default {
   name: "Footer",
@@ -242,11 +241,6 @@ export default {
       },
       localUrl: "",
     };
-  },
-  computed: {
-    copyrightYear() {
-      return moment().format('YYYY')
-    }
   },
   created() {
     this.localUrl = location.origin;
